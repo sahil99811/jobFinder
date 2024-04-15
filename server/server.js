fac6af22127f23fd44e576456e7f6f06e76cb63f
sahil99811 <sp220253@gmail.com>
2024-04-15 23:52:43 +0530

    created a server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(express.json());
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is started");
+})
