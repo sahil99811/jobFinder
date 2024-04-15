bb6de129ba221eabc0e5095964dd612013a5b330
sahil99811 <sp220253@gmail.com>
2024-04-16 00:00:52 +0530

    attach routes to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const app=express();
 const dotenv = require("dotenv");
+const database=require('./config/database');
+const authRoutes=require('./routes/Auth');
+const jobRoutes=require('./routes/Job');
 dotenv.config();
 app.use(express.json());
-
+app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/job',jobRoutes);
 app.listen(process.env.PORT,()=>{
+    database.dbConnect();
     console.log("server is started");
 })
